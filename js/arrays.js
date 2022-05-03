1c4238808320793394a12a22a801c37efcf8136e
Alex <50113949+Dudarik@users.noreply.github.com>
2022-05-03 11:09:07 +0500

    add task

--- js/arrays.js
+++ js/arrays.js
@@ -12,3 +12,33 @@ var sortArrayByParity = function (nums) {
 // let nums = [3, 1, 2, 4];
 // nums = [0];
 // console.log(sortArrayByParity(nums));
+
+/**581. Shortest Unsorted Continuous Subarray */
+/**https://leetcode.com/problems/shortest-unsorted-continuous-subarray/ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function (nums) {
+  const tArr = nums.slice().sort((a, b) => a - b);
+
+  let low = Number.MAX_SAFE_INTEGER,
+    high = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != tArr[i]) {
+      low = Math.min(low, i);
+      high = Math.max(high, i);
+    }
+  }
+
+  return low === Number.MAX_SAFE_INTEGER || high === Number.MIN_SAFE_INTEGER
+    ? 0
+    : high - low + 1;
+};
+
+// let nums = [2, 6, 4, 8, 10, 9, 15];
+// nums = [1];
+// nums = [1, 2, 3, 4];
+// console.log(findUnsortedSubarray(nums));
