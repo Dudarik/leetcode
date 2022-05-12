4d49da2ddd4b5b94d53c9601ca5f2b92fa7b25d0
Alex <50113949+Dudarik@users.noreply.github.com>
2022-05-12 12:21:01 +0500

    add tsak

--- js/strings.js
+++ js/strings.js
@@ -35,16 +35,61 @@ var backspaceCompare = function (s, t) {
   return s.join() === t.join();
 };
 
-let s = "ab#c",
-  t = "ad#c";
-s = "a#c";
-t = "b";
-s = "ab##";
-t = "c#d#";
+// let s = "ab#c",
+//   t = "ad#c";
+// s = "a#c";
+// t = "b";
+// s = "ab##";
+// t = "c#d#";
 
-s = "a##c";
-t = "#a#c";
+// s = "a##c";
+// t = "#a#c";
 
-s = "c##vnvr";
-t = "c##vn#nvr";
-console.log(backspaceCompare(s, t));
+// s = "c##vnvr";
+// t = "c##vn#nvr";
+// console.log(backspaceCompare(s, t));
+
+/**1641. Count Sorted Vowel Strings */
+/**https://leetcode.com/problems/count-sorted-vowel-strings/ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countVowelStrings = function (n) {
+  // let count = 0;
+
+  // const letters = ["a", "e", "i", "o", "u"];
+
+  // const backtrack = (n, pre, index) => {
+  //   if (n === 0 || pre > letters[index]) {
+  //     count += n === 0 ? 1 : 0;
+  //     return;
+  //   }
+  //   for (let i = index; i < letters.length; i++) {
+  //     backtrack(n - 1, letters[i], i);
+  //   }
+  // };
+
+  // backtrack(n, " ", 0);
+  // return count;
+
+  let arr = [1, 1, 1, 1, 1],
+    count = 1,
+    sum = 5,
+    newArr = [1];
+
+  while (count < n) {
+    for (let i = 1; i < 5; i++) {
+      newArr.push(newArr[i - 1] + arr[i]);
+    }
+
+    sum = newArr.reduce((sum, prev) => sum + prev);
+    arr = newArr;
+    newArr = [1];
+    count++;
+  }
+  return sum;
+};
+
+console.log(countVowelStrings(2));
