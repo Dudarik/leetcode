ae43c219921121cb2d7616bb6511c449a1476b71
Alex <Dudarik@yandex.ru>
2022-05-25 23:34:33 +0500

    add task

--- js/arrays.js
+++ js/arrays.js
@@ -112,3 +112,47 @@ function permutations(nums, set, res) {
     }
   }
 }
+
+/**354. Russian Doll Envelopes */
+/**https://leetcode.com/problems/russian-doll-envelopes/ */
+
+/**
+ * @param {number[][]} envelopes
+ * @return {number}
+ */
+
+var maxEnvelopes = function (envelopes) {
+  envelopes.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return b[1] - a[1];
+    }
+    return a[0] - b[0];
+  });
+
+  const tails = [];
+
+  for (let i = 0; i < envelopes.length; i++) {
+    const [w, h] = envelopes[i];
+
+    if (h < tails[0]) {
+      tails[0] = h;
+    } else if (!tails.length || h > tails[tails.length - 1]) {
+      tails.push(h);
+    } else {
+      // find insertion index
+      let l = 0;
+      let r = tails.length - 1;
+      while (l < r) {
+        let mid = ((l + r) / 2) >> 0;
+        if (tails[mid] >= h) {
+          r = mid;
+        } else {
+          l = mid + 1;
+        }
+      }
+      tails[r] = h;
+    }
+  }
+
+  return tails.length;
+};
